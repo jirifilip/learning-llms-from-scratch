5eb14d0a3fe73b51e4e705529de1d3df54c88a52
Jiří Filip <j.f.ilip@seznam.cz>
2026-02-08 18:15:50 +0100

    Create a simple linear model with Pytorch

--- notebooks/2026-02-08-simpe-pytorch-stuff/01-test-out-pytorch.py
+++ notebooks/2026-02-08-simpe-pytorch-stuff/01-test-out-pytorch.py
@@ -18,6 +18,8 @@
 
 # %%
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import seaborn as sns
 
 # %% [markdown]
@@ -71,4 +73,55 @@ p.set_xlabel("parent height")
 p.set_ylabel("child height")
 p
 
+
+# %% [markdown]
+# ## Make a simple linear model
+
 # %%
+class LinearRegression(nn.Module):
+    def __init__(self, feature_size: int):
+        super().__init__()
+        output_size = 1
+
+        # we provide our own bias because I did not remember it can be defined like this
+        nn.
+        self.layer = nn.Linear(feature_size, output_size, bias=False)
+
+    def forward(self, x: torch.tensor):
+        return self.layer(x)
+
+
+# %%
+linear_model = LinearRegression(2)
+print(linear_model)
+
+criterion = nn.MSELoss()
+# small step because we don't have normalized features
+optimizer = torch.optim.SGD(linear_model.parameters(), lr=0.00001)
+
+n_epochs = 20
+
+for epoch in range(n_epochs):
+    optimizer.zero_grad()
+    
+    output = linear_model(feature_matrix)
+    loss = criterion(output, actual_child_height)
+    print(f"{loss = }")
+    
+    loss.backward()
+    optimizer.step()
+
+    print(loss.item())
+
+# %%
+linear_model.layer.weight
+
+# %%
+with torch.no_grad():
+    predicted_child_height = linear_model(feature_matrix)
+
+# %%
+sns.scatterplot(
+    x=predicted_child_height.reshape(-1).numpy(),
+    y=actual_child_height.reshape(-1).numpy()
+)
